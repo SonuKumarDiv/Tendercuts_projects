83aafe486f0120ec07d70b00b551a63d015a9161
SonuKumarDiv <sonukict@gmail.com>
2022-08-20 14:14:57 +0530

    'fix_api'

--- User/serializers.py
+++ User/serializers.py
@@ -8,19 +8,18 @@ class Add_stores_forms(serializers.ModelSerializer):
     class Meta():
         model=models.store_detail
         fields=('__all__')
+        
+class get_stores_forms(serializers.ModelSerializer):
+    class Meta():
+        model=models.store_detail
+        fields=('__all__')
         depth=1
 
 class orders_forms(serializers.ModelSerializer):
     class Meta():
         model=models.order_detail
         fields=('order_name','item_price','total_amount','order_status','address','Order_booked_by','stores')
-        #depth=1
-        #exclude=('Order_booked_by','stores')      
-# class update_orderd_forms(serializers.ModelSerializer):
-#     class Meta():
-#         model=models.order_detail
-#         fields=('__all__')
-
+            
 class update_orderd_forms(serializers.ModelSerializer):
     class Meta():
         model=models.order_detail
